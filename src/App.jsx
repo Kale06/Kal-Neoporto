9f7278fb84ed5d0b20195105292a514bac6a375d
Muhammad Haikal <haikaluwu21@gmail.com>
2024-07-11 04:09:05 +0800

    New stuff!

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,113 @@
-import { Container } from "@mui/material";
+import { Container, Divider, Grid } from "@mui/material";
 import "./App.css";
 import { Layout } from "./Layout";
 import { Kohead } from "./components/Kohead";
 import { useEffect } from "react";
 import anime from "animejs";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaMusic } from "react-icons/fa6";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdOutlineQuestionMark } from "react-icons/md";
 
 function App() {
   useEffect(() => {
-    document.title = "Kal Docs";
+    document.title = "Kal";
+    anime({
+      targets: document.body,
+        backgroundColor: "#f1d9d9",
+      duration: 1000,
+      easing: "easeOutQuad(1, 0.75, 0, 1)",
+    })
     anime({
       targets: ".kohead-div",
       translateY: [180, 0],
       duration: 750,
       easing: "easeOutQuad(1, 0.75, 0, 1)",
     })
+    anime({
+      targets: "h1",
+      translateY: [-100, 0],
+      opacity: [0, 1],
+      duration: 1200,
+      easing: "spring(1, 80, 10, 0)", // adjusted easing for better visual effect
+      delay: anime.stagger(150),
+    });
+    anime({
+      targets: ".kal-grid",
+      translateY: [100, 0],
+      opacity: [0, 1],
+      duration: 1200,
+      easing: 'easeOutCubic',
+      delay: anime.stagger(150),
+    
+    })
   },[]);
+
 
   return (
     <>
       <Container maxWidth="lg">
         <Layout>
           <div>
-            <p>Test</p>
+            <div>
+            <h1 className="highlight">Kal</h1>
+            </div>
+            <div style={{
+              padding:'10px'
+            }} className="kal-grid">
+            <Divider>
+              <FaLocationDot />
+            </Divider>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <p>ID - Tangerang </p>
+              </Grid>
+              <Grid item xs={12} md={6}>
+              <p>MY - Selangor</p>
+              </Grid>
+            </Grid>
+            </div>
+            <div style={{
+              padding:'10px'
+            }} className="kal-grid">
+            <Divider><MdOutlineQuestionMark /> </Divider>
+            <p style={{
+              maxWidth:'500px'
+            }}>IT Student concentrating in Software Engineering, I like twiddling with computers, listen to metal music, and scrounging through teh interwebz. Aspiring to work as a System Architect, DevOps, Cloud Engineer, or Software Engineer.</p>
+            </div>
+            <div style={{
+              padding:'10px'
+            }} className="kal-grid">
+            <Divider>
+            <h4>Poke me</h4>
+            </Divider>
+            <p style={{
+              fontSize:'10pt'
+            }}>(As in metaphorically, not in a literal sense, you get what I'm trying to say. ....Right?)</p>
+            <div style={{
+              display:'flex',
+              alignItems:'center',
+              justifyContent:'space-evenly',
+              margin:'15px 0',
+            }}>
+              <button name="linkedin" onClick={
+                () => {
+                  window.open("https://www.linkedin.com/in/kaaal/")
+                }
+              } className="invisible">
+                    <FaLinkedin size={40}/>
+              </button>
+              <button onClick={() => {window.open("https://github.com/Kale06")}} className="invisible">
+                    <FaGithubAlt size={40}/>
+              </button>
+              <button onClick={() => {window.open("mailto:muhammad.haikal100602@gmail.com")}} className="invisible">
+                    <MdEmail size={40}/>
+              </button>
+            </div>
+            
+          </div>
           </div>
         </Layout>
         <Kohead />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,103 +5,26 @@ import anime from "animejs";
 export function Header() {
   useEffect(() => {
     anime({
-      targets: ".header-items",
-      translateY: [-100, 0],
-      duration: 1200,
-      easing: "spring(1, 80, 10, 0)", // adjusted easing for better visual effect
-      delay: anime.stagger(150),
-    });
-  }, []);
-
-
-  function animateRoot() {
-    anime({
-      targets: document.body,
-        backgroundColor: "#2f323a",
-      duration: 750,
-      easing: "easeOutQuad(1, 0.75, 0, 1)",
+      targets: ".kal-header",
+      translateY: [-50, 0],
+      opacity: [0, 1],
+      duration: 500,
+      easing: "easeOutQuad", 
     })
-  }
+  }, []);
 
   return (
     <header>
       <Container maxWidth="lg">
-        <ul className="desktop">
-          <li className="header-items">
-            <button 
-            onClick={animateRoot}
-            className="invisible">
-              <h2
-                style={{
-                  lineHeight: "0.8",
-                  textAlign: "left",
-                }}
-                className="highlight"
-              >
-                Muhammad
-                <br />
-                Haikal
-              </h2>
-            </button>
-          </li>
-          <li
-            className="header-items"
-            style={{
-              textAlign: "left",
-            }}
-          >
-            <p>ID-Jakarta/</p>
-            <p>MY-Selangor</p>
-          </li>
-          <li
-            className="header-items"
-            style={{
-              textAlign: "left",
-            }}
-          >
-            <p>Poke me</p>
-            <div
-              style={{
-                display: "flex",
-                width: "100px",
-                justifyContent: "space-between",
-              }}
-            >
-              <div className="item">
-                <p>LinkedIn</p>
-                <div className="line" />
-              </div>
-              <div className="item">
-                <p>Email</p>
-                <div className="line" />
-              </div>
-            </div>
-          </li>
-          <li
-            className="header-items"
-            style={{
-              textAlign: "right",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-end",
-            }}
-          >
-            <div>
-              <div className="item">
-                <p>About me & purpose of this web</p>
-                <div className="line" />
-              </div>
-            </div>
-            <div className="item">
-              <p>Stuff I co-/created</p>
-              <div className="line" />
-            </div>
-            <div className="item">
-              <p>My Spotify Playlist</p>
-              <div className="line" />
-            </div>
-          </li>
-        </ul>
+        <p className="kal-header" style={{
+          padding:'15px',
+          fontSize:'10pt',
+          color:'var(--teal)',
+        }}>This website was lovingly made using <b style={{
+          color:'black'
+        }}>React, AnimeJS, MUI,</b> and <b style={{
+          color:'black'
+        }}>Vercel</b></p>
       </Container>
     </header>
   );
