5e06e514289301d26e0d3110b31bb0fd6ec1e524
Muhammad Haikal <98247943+haikaaal21@users.noreply.github.com>
2024-07-21 20:08:39 +0800

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ import anime from "animejs";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithubAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import { FaMusic } from "react-icons/fa6";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdOutlineQuestionMark } from "react-icons/md";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   useEffect(() => {
     document.title = "Kal";
     anime({
       targets: document.body,
-        backgroundColor: "#f1d9d9",
+      backgroundColor: "#f1d9d9",
       duration: 1000,
       easing: "easeOutQuad(1, 0.75, 0, 1)",
     })
@@ -41,9 +41,9 @@ function App() {
       duration: 1200,
       easing: 'easeOutCubic',
       delay: anime.stagger(150),
-    
+
     })
-  },[]);
+  }, []);
 
 
   return (
@@ -52,66 +52,67 @@ function App() {
         <Layout>
           <div>
             <div>
-            <h1 className="highlight">Kal</h1>
+              <h1 className="highlight">Kal</h1>
             </div>
             <div style={{
-              padding:'10px'
+              padding: '10px'
             }} className="kal-grid">
-            <Divider>
-              <FaLocationDot />
-            </Divider>
-            <Grid container>
-              <Grid item xs={12} md={6}>
-                <p>ID - Tangerang </p>
+              <Divider>
+                <FaLocationDot />
+              </Divider>
+              <Grid container>
+                <Grid item xs={12} md={6}>
+                  <p>ID - Tangerang </p>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <p>MY - Selangor</p>
+                </Grid>
               </Grid>
-              <Grid item xs={12} md={6}>
-              <p>MY - Selangor</p>
-              </Grid>
-            </Grid>
             </div>
             <div style={{
-              padding:'10px'
+              padding: '10px'
             }} className="kal-grid">
-            <Divider><MdOutlineQuestionMark /> </Divider>
-            <p style={{
-              maxWidth:'500px'
-            }}>IT Student concentrating in Software Engineering, I like twiddling with computers, listen to metal music, and scrounging through teh interwebz. Aspiring to work as a System Architect, DevOps, Cloud Engineer, or Software Engineer.</p>
+              <Divider><MdOutlineQuestionMark /> </Divider>
+              <p style={{
+                maxWidth: '500px'
+              }}>IT Student concentrating in Software Engineering, I like twiddling with computers, listen to metal music, and scrounging through teh interwebz. Aspiring to work as a System Architect, DevOps, Cloud Engineer, or Software Engineer.</p>
             </div>
             <div style={{
-              padding:'10px'
+              padding: '10px'
             }} className="kal-grid">
-            <Divider>
-            <h4>Poke me</h4>
-            </Divider>
-            <p style={{
-              fontSize:'10pt'
-            }}>(As in metaphorically, not in a literal sense, you get what I'm trying to say. ....Right?)</p>
-            <div style={{
-              display:'flex',
-              alignItems:'center',
-              justifyContent:'space-evenly',
-              margin:'15px 0',
-            }}>
-              <button name="linkedin" onClick={
-                () => {
-                  window.open("https://www.linkedin.com/in/kaaal/")
-                }
-              } className="invisible">
-                    <FaLinkedin size={40}/>
-              </button>
-              <button onClick={() => {window.open("https://github.com/Kale06")}} className="invisible">
-                    <FaGithubAlt size={40}/>
-              </button>
-              <button onClick={() => {window.open("mailto:muhammad.haikal100602@gmail.com")}} className="invisible">
-                    <MdEmail size={40}/>
-              </button>
+              <Divider>
+                <h4>Poke me</h4>
+              </Divider>
+              <p style={{
+                fontSize: '10pt'
+              }}>(As in metaphorically, not in a literal sense, you get what I'm trying to say. ....Right?)</p>
+              <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-evenly',
+                margin: '15px 0',
+              }}>
+                <button name="linkedin" onClick={
+                  () => {
+                    window.open("https://www.linkedin.com/in/kaaal/")
+                  }
+                } className="invisible">
+                  <FaLinkedin size={40} />
+                </button>
+                <button onClick={() => { window.open("https://github.com/Kale06") }} className="invisible">
+                  <FaGithubAlt size={40} />
+                </button>
+                <button onClick={() => { window.open("mailto:muhammad.haikal100602@gmail.com") }} className="invisible">
+                  <MdEmail size={40} />
+                </button>
+              </div>
+
             </div>
-            
-          </div>
           </div>
         </Layout>
         <Kohead />
       </Container>
+      <Analytics />
     </>
   );
 }
