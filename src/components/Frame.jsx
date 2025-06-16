379cb585d04533b6ccf4b43c1169b89b958bcd2e
Muhammad Haikal <haikaluwu21@gmail.com>
2025-06-17 06:39:29 +0700

    Lefty addition

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,8 +1,10 @@
 export function Frame({children}){
+
     return (
-        <div style={{
+        <div className="frame" style={{
             width:"100%",
             border:"1px solid white",
+            padding:"25px 10px"
         }}>
             {children}
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Container } from "@mui/material";
+import { Container, Divider } from "@mui/material";
 import "../App.css";
 import { useEffect } from "react";
 import anime from "animejs";
@@ -10,6 +10,7 @@ import { MdOutlineQuestionMark } from "react-icons/md";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Frame } from "../components/Frame";
+import { Barrier } from "../components/barrier";
 
 function Home() {
   useEffect(() => {
@@ -29,12 +30,30 @@ function Home() {
         display:"flex"
       }} maxWidth="lg">
         <div id="lefty" className="containerItem">
+            <h4>Current status</h4>
+            <h3>Open to work</h3>
             <p>About Me</p>
         </div>
         <div id="righty" className="containerItem">
         <Frame>
-            <h1>Howdy,</h1>
-            <h1>I'm Haikal.</h1>
+            <h1>Howdy</h1>
+            <h1>there stranger.</h1>
+            <Barrier />
+            <p>
+                I'm Muhammad Haikal, a fresh graduate Information
+                Technology student from Universiti Utara Malaysia.
+                My field of concentration is Software Engineering
+                and I mostly dabble in Java, Javascript/Typescript,
+                and Bash. But currently I'm trying to dabble more on
+                Rust and Ansible for future work/hobbies/projects.
+            </p>
+            <Barrier />
+            <Divider component="li" variant="fullwidth" />
+            <h1>Work</h1>
+            <Divider component="li" variant="fullwidth" />
+            <h1>Projects</h1>
+            <Divider component="li" variant="fullwidth" />
+            <h1>Tools</h1>
         </Frame>
         </div>
       </Container>
