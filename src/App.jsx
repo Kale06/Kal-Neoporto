4efb0dae456826584b0a3e0194f8dc58d650712b
Muhammad Haikal <haikaluwu21@gmail.com>
2025-06-17 23:38:43 +0700

    Deployment hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from "./pages/home";
 import { Header } from "./components/Header";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Footer } from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
 
