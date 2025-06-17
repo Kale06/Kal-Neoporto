76b07439116252120fcc85b670f98a8f7fbdb49c
Kal <98247943+Kale06@users.noreply.github.com>
2025-06-17 23:40:51 +0700

    Hotfix, typeerror

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { Header } from "./components/Header";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/react';
